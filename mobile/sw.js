187d0b47886e493a819b931d50fda84e2f57ab89
azkargorta <azkardurant@gmail.com>
2026-09-06 16:07:36 +0200

    Add offline weekly planner and training goals

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vitalpeak-mobile-v4";
+const CACHE = "vitalpeak-mobile-v5";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./catalog-data.js", "./app.js", "./manifest.webmanifest", "./icon-cover.png"];
 
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
