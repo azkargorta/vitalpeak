1a2fa685935c3174ef967e9f46f38a43f80bf527
azkargorta <azkardurant@gmail.com>
2026-09-07 10:07:20 +0200

    Refresh PWA cache for rest day home state

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vitalpeak-mobile-v18";
+const CACHE = "vitalpeak-mobile-v19";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./catalog-data.js", "./app.js", "./routine-enhancements.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", event => event.waitUntil(
