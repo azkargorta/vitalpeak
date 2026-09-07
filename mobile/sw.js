a8532993f5335e54e200b29aa3470fe83b6b128d
azkargorta <azkardurant@gmail.com>
2026-09-07 09:22:07 +0200

    Refresh PWA cache for calendar navigation

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vitalpeak-mobile-v15";
+const CACHE = "vitalpeak-mobile-v16";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./catalog-data.js", "./app.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", event => event.waitUntil(
