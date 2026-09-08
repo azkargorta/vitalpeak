092d18b38875b1c818360e79f49971e8a5a5114e
azkargorta <azkardurant@gmail.com>
2026-09-08 13:40:12 +0200

    Actualiza caché PWA con imágenes de cardio

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,5 +1,17 @@
-const CACHE = "vitalpeak-mobile-v41";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./cardio-training.js", "./cardio-ui-polish.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png"];
+const CACHE = "vitalpeak-mobile-v42";
+const APP_SHELL = [
+  "./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./cardio-training.js", "./cardio-ui-polish.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png",
+  "./cardio-media/cinta-de-correr.svg",
+  "./cardio-media/caminata-cinta-inclinacion.svg",
+  "./cardio-media/bicicleta-estatica.svg",
+  "./cardio-media/bicicleta-aire.svg",
+  "./cardio-media/bicicleta-eliptica.svg",
+  "./cardio-media/remo-ergometro.svg",
+  "./cardio-media/escaladora.svg",
+  "./cardio-media/saltar-comba.svg",
+  "./cardio-media/carrera-exterior.svg",
+  "./cardio-media/caminata-rapida.svg"
+];
 
 self.addEventListener("install", event => event.waitUntil(
   caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
