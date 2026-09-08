8e55e2fad1ed48515aeee6be651ec9bd4cb85abe
azkargorta <azkardurant@gmail.com>
2026-09-08 08:51:54 +0200

    Actualiza caché PWA para entrenamiento cardio

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "vitalpeak-mobile-v39";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png"];
+const CACHE = "vitalpeak-mobile-v40";
+const APP_SHELL = ["./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./cardio-training.js", "./cardio-ui-polish.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", event => event.waitUntil(
   caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
