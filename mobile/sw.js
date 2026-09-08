fafe68a95ff6878ae0099fb430ed247e576df350
azkargorta <azkardurant@gmail.com>
2026-09-08 20:39:07 +0200

    Actualiza caché para imágenes cardio y filtros persistentes

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,6 +1,6 @@
-const CACHE = "vitalpeak-mobile-v44";
+const CACHE = "vitalpeak-mobile-v45";
 const APP_SHELL = [
-  "./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./cardio-training.js", "./cardio-ui-polish.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png",
+  "./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./cardio-training.js", "./cardio-ui-polish.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./routine-filter-persistence.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png",
   "./cardio-media/cinta-de-correr.svg",
   "./cardio-media/caminata-cinta-inclinacion.svg",
   "./cardio-media/bicicleta-estatica.svg",
