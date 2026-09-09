33e76f77879ac03763b554cb2db37f57e0285fcc
azkargorta <azkardurant@gmail.com>
2026-09-09 09:10:11 +0200

    Actualiza caché para GIF e imágenes cardio

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vitalpeak-mobile-v49";
+const CACHE = "vitalpeak-mobile-v50";
 const APP_SHELL = [
   "./", "./index.html", "./styles.css", "./catalog-data.js", "./cardio-catalog.js", "./cardio-training.js", "./cardio-ui-polish.js", "./app.js", "./routine-enhancements.js", "./routine-muscle-filter.js", "./calendar-mobile.js", "./training-intelligence.js", "./routine-navigation-fix.js", "./routines-accordion.js", "./routine-filter-persistence.js", "./training-set-editor.js", "./manifest.webmanifest", "./icon-cover.png", "./icons/icon-192.svg", "./apple-touch-icon.png",
   "./cardio-media/cinta-de-correr.svg",
@@ -10,13 +10,7 @@ const APP_SHELL = [
   "./cardio-media/escaladora.svg",
   "./cardio-media/saltar-comba.svg",
   "./cardio-media/carrera-exterior.svg",
-  "./cardio-media/caminata-rapida.svg",
-  "./cardio-media/cinta-de-correr.webp",
-  "./cardio-media/caminata-cinta-inclinacion.webp",
-  "./cardio-media/bicicleta-estatica.webp",
-  "./cardio-media/bicicleta-aire.webp",
-  "./cardio-media/bicicleta-eliptica.webp",
-  "./cardio-media/remo-ergometro.webp"
+  "./cardio-media/caminata-rapida.svg"
 ];
 
 self.addEventListener("install", event => event.waitUntil(
